dfe13f3a699ebc48cb4846a22115288191f12132
shihab <ahmedshihab7@gmail.com>
2026-01-03 19:06:08 +0600

    Day 1: Read and display image using OpenCV

---
+++ week_01_image_basics/day_01_read_image.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+
+# read image
+image = cv.imread('jack_jones.png')
+
+if image is None :
+   print("No image found")
+   exit(0)
+
+# show image
+cv.imshow('jack_jones', image)
+
+# printing details
+print(image.shape)
+print(image.dtype)
+
+# convert image to grayscale
+gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+cv.imshow('jack_jones_grayscale', gray_image)
+
+# printing grayscale image details
+print(gray_image.shape)
+print(gray_image.dtype)
+
+# displaying red dot in color image
+image[400, 250] = (0, 0, 255)
+cv.imshow('jack_jones_grayscale_red_dot', image)
+
+# displaying read circle in grayscale image
+cv.circle(gray_image, (400,250), 10, (0,0,255), -1)
+cv.imshow('jack_jones_grayscale_red_circle', gray_image)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
